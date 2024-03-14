a8818b2fb94543d9aef7e96cf52b66dc128a7db9
Nguyễn Trường Tho <truongthonguyen107@gmail.com>
2024-03-14 20:18:02 +0700

    feat request
    SWP-71 #back-end completed
    SWP-72 #back-end completed
    SWP-73 #back-end completed
    SWP-74 #back-end completed

--- src/main/java/com/cowards/onlyarts/resources/v4/Request.java
+++ src/main/java/com/cowards/onlyarts/resources/v4/Request.java
@@ -1,9 +1,26 @@
 package com.cowards.onlyarts.resources.v4;
 
+import com.cowards.onlyarts.core.CodeGenerator;
+import com.cowards.onlyarts.repositories.request.RequestDTO;
+import com.cowards.onlyarts.repositories.request.RequestERROR;
+import com.cowards.onlyarts.repositories.token.TokenERROR;
+import com.cowards.onlyarts.repositories.user.UserDTO;
+import com.cowards.onlyarts.repositories.user.UserERROR;
 import com.cowards.onlyarts.services.RequestDAO;
 import com.cowards.onlyarts.services.TokenDAO;
 import com.cowards.onlyarts.services.UserDAO;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
 
 @Path("v4/requests")
 public class Request {
@@ -11,4 +28,209 @@ public class Request {
     private final RequestDAO requestDAO = RequestDAO.getInstance();
     private final TokenDAO tokenDao = TokenDAO.getInstance();
     private final UserDAO userDAO = UserDAO.getInstance();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllRequests(@HeaderParam("authtoken") String tokenString) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            String roleID = userDAO.getUserById(userId).getRoleId();
+            if ("CT".equalsIgnoreCase(roleID) || "CR".equalsIgnoreCase(roleID)) {
+                List<RequestDTO> requestList = requestDAO.getAllRequest(userId, roleID);
+                return !requestList.isEmpty()
+                        ? Response.status(Response.Status.OK).entity(requestList).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new UserERROR("You do not have permission");
+            }
+        } catch (TokenERROR | UserERROR ex) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity(ex).build();
+        }
+    }
+
+    @GET
+    @Path("{request_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRequestByID(@HeaderParam("authtoken") String tokenString,
+            @PathParam("request_id") String requestId) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            RequestDTO request = requestDAO.getRequestById(requestId);
+            if (userId.equalsIgnoreCase(request.getCustomerID()) || userId.equalsIgnoreCase(request.getPublisherId())) {
+                return Response.status(Response.Status.OK)
+                        .entity(request)
+                        .build();
+            } else {
+                throw new TokenERROR("You do not have permission to see this request");
+            }
+
+        } catch (TokenERROR | RequestERROR ex) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(ex)
+                    .build();
+        }
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createRequest(@HeaderParam("authtoken") String tokenString,
+            RequestDTO request) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            UserDTO user = userDAO.getUserById(userId);
+            if ("CT".equalsIgnoreCase(user.getRoleId())) {
+                String requestId = CodeGenerator.generateUUID(20);
+                request.setCustomerID(userId);
+                request.setRequestId(requestId);
+                boolean checkInsert = requestDAO.addRequest(request);
+                return checkInsert
+                        ? Response.status(Response.Status.OK).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new UserERROR("Only customer can request");
+            }
+        } catch (TokenERROR | UserERROR e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e)
+                    .build();
+        }
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateRequest(@HeaderParam("authtoken") String tokenString,
+            RequestDTO request) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            if (userId.equalsIgnoreCase(request.getCustomerID())) {
+                boolean checkUpdate = requestDAO.updateRequest(request);
+                return checkUpdate
+                        ? Response.status(Response.Status.OK).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new TokenERROR("You do not have permission to update this request");
+            }
+        } catch (TokenERROR e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e)
+                    .build();
+        }
+    }
+
+    @DELETE
+    @Path("{request_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteRequest(@HeaderParam("authtoken") String tokenString,
+            @PathParam("request_id") String requestId) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            RequestDTO request = requestDAO.getRequestById(requestId);
+            if (userId.equalsIgnoreCase(request.getCustomerID())) {
+                boolean checkDelete = requestDAO.removeRequest(requestId);
+                return checkDelete
+                        ? Response.status(Response.Status.OK).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new RequestERROR("You do not have permission to delete this request");
+            }
+        } catch (TokenERROR | RequestERROR e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e)
+                    .build();
+        }
+    }
+
+    @PUT
+    @Path("approve/{request_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response approveRequest(@HeaderParam("authtoken") String tokenString,
+            @PathParam("request_id") String requestId) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            RequestDTO request = requestDAO.getRequestById(requestId);
+            if (userId.equalsIgnoreCase(request.getPublisherId())) {
+                boolean check = requestDAO.changeStatus(request, 0b0010);
+                return check
+                        ? Response.status(Response.Status.OK).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new RequestERROR("You do not have permission to approve/reject this request");
+            }
+        } catch (TokenERROR | RequestERROR e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e)
+                    .build();
+        }
+    }
+
+    @PUT
+    @Path("seen/{request_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response readRequest(@HeaderParam("authtoken") String tokenString,
+            @PathParam("request_id") String requestId) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            RequestDTO request = requestDAO.getRequestById(requestId);
+            if (userId.equalsIgnoreCase(request.getPublisherId())) {
+                boolean check = requestDAO.changeStatus(request, 0b1000);
+                return check
+                        ? Response.status(Response.Status.OK).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new RequestERROR("You do not have permission to seen/unseen this request");
+            }
+        } catch (TokenERROR | RequestERROR e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e)
+                    .build();
+        }
+    }
+
+    @PUT
+    @Path("response/{request_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response rejectRequest(@HeaderParam("authtoken") String tokenString,
+            @PathParam("request_id") String requestId) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            RequestDTO request = requestDAO.getRequestById(requestId);
+            if (userId.equalsIgnoreCase(request.getPublisherId())) {
+                boolean check = requestDAO.changeStatus(request, 0b0100);
+                return check
+                        ? Response.status(Response.Status.OK).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new RequestERROR("You do not have permission to response this request");
+            }
+        } catch (TokenERROR | RequestERROR e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e)
+                    .build();
+        }
+    }
+
+    @PUT
+    @Path("remove/{request_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response doneRequest(@HeaderParam("authtoken") String tokenString,
+            @PathParam("request_id") String requestId) {
+        try {
+            String userId = tokenDao.getToken(tokenString).getUserId();
+            RequestDTO request = requestDAO.getRequestById(requestId);
+            if (userId.equalsIgnoreCase(request.getPublisherId())) {
+                boolean check = requestDAO.changeStatus(request, 0b0001);
+                return check
+                        ? Response.status(Response.Status.OK).build()
+                        : Response.status(Response.Status.NO_CONTENT).build();
+            } else {
+                throw new RequestERROR("You do not have permission to remove this request");
+            }
+        } catch (TokenERROR | RequestERROR e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e)
+                    .build();
+        }
+    }
 }
--- src/main/java/com/cowards/onlyarts/services/RequestDAO.java
+++ src/main/java/com/cowards/onlyarts/services/RequestDAO.java
@@ -1,6 +1,15 @@
 package com.cowards.onlyarts.services;
 
 import com.cowards.onlyarts.core.DBContext;
+import com.cowards.onlyarts.repositories.request.RequestDTO;
+import com.cowards.onlyarts.repositories.request.RequestERROR;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,6 +17,24 @@ public class RequestDAO {
 
     private static RequestDAO instance;
     private static final DBContext DB = DBContext.getInstance();
+    private static final String GET_ALL_REQUESTS_BY_CR = "SELECT [customer_id],[publisher_id],"
+            + "[request_time],[description],[status],[request_id] FROM [dbo].[Requests] "
+            + "WHERE [publisher_id] = ?";
+    private static final String GET_ALL_REQUESTS_BY_CT = "SELECT [customer_id],[publisher_id],"
+            + "[request_time],[description],[status],[request_id] FROM [dbo].[Requests] "
+            + "WHERE [customer_id] = ?";
+    private static final String GET_REQUEST_BY_ID = "SELECT [customer_id],[publisher_id],"
+            + "[request_time],[description],[status],[request_id] FROM [dbo].[Requests] "
+            + "WHERE [request_id] = ?";
+    private static final String ADD_REQUEST = "INSERT INTO [dbo].[Requests] "
+            + "([request_id],[customer_id],[publisher_id],[description],[status]) "
+            + "VALUES (?,?,?,?,?)";
+    private static final String UPDATE_REQUEST = "UPDATE [dbo].[Requests] "
+            + "SET [description] = ?, [status] = ? WHERE request_id = ?";
+    private static final String REMOVE_REQUEST = "DELETE FROM [dbo].[Requests] "
+            + "WHERE request_id = ?";
+    private static final String CHANGE_STATUS = "UPDATE [dbo].[Requests] "
+            + "SET [status] = ? WHERE request_id = ?";
 
     public static RequestDAO getInstance() {
         if (instance == null) {
@@ -19,6 +46,160 @@ public class RequestDAO {
     private void logError(String message, Exception ex) {
         Logger.getLogger(UserDAO.class.getName())
                 .log(Level.SEVERE, message, ex);
+    }
+
+    public List<RequestDTO> getAllRequest(String userId, String roleID) {
+        List<RequestDTO> requestList = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DB.getConnection();
+            if ("CR".equals(roleID)) {
+                stm = conn.prepareStatement(GET_ALL_REQUESTS_BY_CR);
+            } else if ("CT".equalsIgnoreCase(roleID)) {
+                stm = conn.prepareStatement(GET_ALL_REQUESTS_BY_CT);
+            } else {
+                return null;
+            }
+            stm.setString(1, userId);
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                RequestDTO request = new RequestDTO();
+                request.setRequestId(rs.getString("request_id"));
+                request.setCustomerID(rs.getString("customer_id"));
+                request.setPublisherId(rs.getString("publisher_id"));
+                request.setRequestTime(new Date(rs.getTimestamp("request_time").getTime()));
+                request.setDescription(rs.getString("description"));
+                request.setStatus(rs.getInt("status"));
+                requestList.add(request);
+            }
+        } catch (SQLException ex) {
+            logError("Exception found on getAllRequest() method", ex);
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(stm);
+        }
+        return requestList;
+    }
+
+    public RequestDTO getRequestById(String requestId) throws RequestERROR {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        RequestDTO request = null;
+        try {
+            conn = DB.getConnection();
+            stm = conn.prepareStatement(GET_REQUEST_BY_ID);
+            stm.setString(1, requestId);
+            rs = stm.executeQuery();
+            if (rs.next()) {
+                request = new RequestDTO();
+                request.setRequestId(rs.getString("request_id"));
+                request.setCustomerID(rs.getString("customer_id"));
+                request.setPublisherId(rs.getString("publisher_id"));
+                request.setRequestTime(rs.getDate("request_time"));
+                request.setDescription(rs.getString("description"));
+                request.setStatus(rs.getInt("status"));
+            } else {
+                throw new RequestERROR("This id does not exist in the system");
+            }
+        } catch (SQLException ex) {
+            logError("Exception found on getUserById() method", ex);
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(stm);
+        }
+        return request;
+    }
+
+    public boolean addRequest(RequestDTO request) {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        try {
+            conn = DB.getConnection();
+            if (conn != null) {
+                stm = conn.prepareStatement(ADD_REQUEST);
+                stm.setString(1, request.getRequestId());
+                stm.setString(2, request.getCustomerID());
+                stm.setString(3, request.getPublisherId());
+                stm.setString(4, request.getDescription());
+                stm.setInt(5, 0);
+                check = stm.executeUpdate() > 0;
+            }
+        } catch (SQLException e) {
+            logError("Exception found on addRequest() method", e);
+        } finally {
+            DB.closeStatement(stm);
+        }
+        return check;
+
+    }
+
+    public boolean updateRequest(RequestDTO request) {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        try {
+            conn = DB.getConnection();
+            if (conn != null) {
+                stm = conn.prepareStatement(UPDATE_REQUEST);
+                stm.setString(1, request.getDescription());
+                if (request.isSeen()) {
+                    stm.setInt(2, request.getStatus() ^ 0b1000);
+                } else {
+                    stm.setInt(2, request.getStatus());
+                }
+                stm.setString(3, request.getRequestId());
+
+                check = stm.executeUpdate() > 0;
+            }
+        } catch (SQLException e) {
+            logError("Exception found on updateRequest() method", e);
+        } finally {
+            DB.closeStatement(stm);
+        }
+        return check;
+    }
+
+    public boolean removeRequest(String requestId) {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        try {
+            conn = DB.getConnection();
+            if (conn != null) {
+                stm = conn.prepareStatement(REMOVE_REQUEST);
+                stm.setString(1, requestId);
+                check = stm.executeUpdate() > 0;
+            }
+        } catch (SQLException e) {
+            logError("Exception found on removeRequest() method", e);
+        } finally {
+            DB.closeStatement(stm);
+        }
+        return check;
+    }
+
+    public boolean changeStatus(RequestDTO request, int state) {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        try {
+            conn = DB.getConnection();
+            if (conn != null) {
+                stm = conn.prepareStatement(CHANGE_STATUS);
+                stm.setInt(1, request.getStatus() ^ state);
+                stm.setString(2, request.getRequestId());
+                check = stm.executeUpdate() > 0;
+            }
+        } catch (SQLException e) {
+            logError("Exception found on changStatus() method", e);
+        } finally {
+            DB.closeStatement(stm);
+        }
+        return check;
     }
 
 }
