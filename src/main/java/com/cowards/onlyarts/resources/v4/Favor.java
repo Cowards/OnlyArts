209ea60ca3de4974933b2fb2afd28b2874f48958
Nguyễn Trường Tho <truongthonguyen107@gmail.com>
2024-03-14 20:09:21 +0700

    feat favor
    SWP-82 #back-end completed

--- src/main/java/com/cowards/onlyarts/resources/v4/Favor.java
+++ src/main/java/com/cowards/onlyarts/resources/v4/Favor.java
@@ -1,9 +1,21 @@
 package com.cowards.onlyarts.resources.v4;
 
+import com.cowards.onlyarts.repositories.artwork.ArtworkDTO;
+import com.cowards.onlyarts.repositories.token.TokenDTO;
+import com.cowards.onlyarts.repositories.token.TokenERROR;
+import com.cowards.onlyarts.repositories.user.UserDTO;
+import com.cowards.onlyarts.repositories.user.UserERROR;
 import com.cowards.onlyarts.services.FavorDAO;
 import com.cowards.onlyarts.services.TokenDAO;
 import com.cowards.onlyarts.services.UserDAO;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
 
 @Path("v4/favor")
 public class Favor {
@@ -12,4 +24,28 @@ public class Favor {
     private static final TokenDAO tokenDao = TokenDAO.getInstance();
     private static final UserDAO userDao = UserDAO.getInstance();
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFavoriteArtworks(@HeaderParam("authtoken") String tokenString) {
+        try {
+            TokenDTO token = tokenDao.getToken(tokenString);
+            List<ArtworkDTO> artworks = favorDao.getFavoriteArtworks(token.getUserId());
+            return Response.status(200).entity(artworks).build();
+        } catch (TokenERROR ex) {
+            return Response.status(401).entity(ex).build();
+        }
+    }
+
+    @GET
+    @Path("{userid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUserFavoriteArtwroks(@PathParam("userid") String userId) {
+        try {
+            UserDTO user = userDao.getUserById(userId);
+            List<ArtworkDTO> artworks = favorDao.getFavoriteArtworks(user.getUserId());
+            return Response.status(200).entity(artworks).build();
+        } catch (UserERROR ex) {
+            return Response.status(404).entity(ex).build();
+        }
+    }
 }
--- src/main/java/com/cowards/onlyarts/services/FavorDAO.java
+++ src/main/java/com/cowards/onlyarts/services/FavorDAO.java
@@ -1,6 +1,13 @@
 package com.cowards.onlyarts.services;
 
 import com.cowards.onlyarts.core.DBContext;
+import com.cowards.onlyarts.repositories.artwork.ArtworkDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,6 +15,20 @@ public class FavorDAO {
 
     private static final DBContext context = DBContext.getInstance();
     private static FavorDAO instance;
+    //
+    private static final String ADD_FAVORITE
+            = "INSERT INTO [dbo].[Users_favor]"
+            + " ([user_id], [artwork_id]) VALUES(?, ?)";
+    //
+    private static final String REMOVE_FAVORITE
+            = "DELETE FROM [dbo].[Users_favor]"
+            + "WHERE [user_id] = ? AND [artwork_id] = ?";
+    private static final String GET_FAVORITE_ARTWORKS
+            = "SELECT [dbo].[Artworks].[artwork_id],[owner_id],[cate_id],[name],"
+            + " [description],[artwork_image], [price], [released_date], [status]"
+            + " FROM [dbo].[Artworks] JOIN [dbo].[Users_favor] ON"
+            + " [dbo].[Artworks].[artwork_id] = [dbo].[Users_favor].[artwork_id]"
+            + " WHERE [dbo].[Users_favor].[user_id] = ?";
 
     private FavorDAO() {
     }
@@ -22,5 +43,78 @@ public class FavorDAO {
             instance = new FavorDAO();
         }
         return instance;
+    }
+
+    public boolean removeFavorite(String userId, String artworkId) {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        boolean res = false;
+        try {
+            conn = context.getConnection();
+            stm = conn.prepareStatement(REMOVE_FAVORITE);
+            stm.setString(1, userId);
+            stm.setString(2, artworkId);
+            res = stm.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            logError("Exception found on addReaction() method", ex);
+            res = false;
+        } finally {
+            context.closeStatement(stm);
+        }
+        return res;
+    }
+
+    //
+    public boolean addFavorite(String userId, String artworkId) {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        boolean res = false;
+        try {
+            conn = context.getConnection();
+            stm = conn.prepareStatement(ADD_FAVORITE);
+            stm.setString(1, userId);
+            stm.setString(2, artworkId);
+            res = stm.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            logError("Exception found on addFavorite() method", ex);
+            res = false;
+        } finally {
+            context.closeStatement(stm);
+        }
+        return res;
+    }
+    //
+
+    public List<ArtworkDTO> getFavoriteArtworks(String userId) {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        List<ArtworkDTO> artworks = new ArrayList<>();
+        try {
+            conn = context.getConnection();
+            stm = conn.prepareStatement(GET_FAVORITE_ARTWORKS);
+            stm.setString(1, userId);
+            rs = stm.executeQuery();
+            ArtworkDTO artwork = null;
+            while (rs.next()) {
+                artwork = new ArtworkDTO();
+                artwork.setArtworkId(rs.getString(1));
+                artwork.setOwnerId(rs.getString(2));
+                artwork.setCateId(rs.getString(3));
+                artwork.setName(rs.getString(4));
+                artwork.setDescription(rs.getString(5));
+                artwork.setArtworkImage(rs.getString(6));
+                artwork.setPrice(rs.getFloat(7));
+                artwork.setReleasedDate(rs.getDate(8));
+                artwork.setStatus(rs.getInt(9));
+                artworks.add(artwork);
+            }
+        } catch (SQLException ex) {
+            logError("Exception found on getFavoriteArtworks() method", ex);
+        } finally {
+            context.closeResultSet(rs);
+            context.closeStatement(stm);
+        }
+        return artworks;
     }
 }
