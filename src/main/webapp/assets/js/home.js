5e9af40d7b8f25409d7ce6afcecc3f3d13046e8a
Neru <84850523+thucnee511@users.noreply.github.com>
2024-03-15 06:19:06 +0700

    feat home

--- src/main/webapp/assets/js/home.js
+++ src/main/webapp/assets/js/home.js
@@ -1,49 +1,11 @@
-const main = document.querySelector("#artworks");
-const loading = async () => {
-  try {
-    const artworks = await sendJson("GET", `${api}/api/v2/artworks`);
-    artworks.forEach(async (element) => {
-      const imageData = await send(
-        "GET",
-        `${api}/api/v1/image/${element.artworkImage}`
-      );
-      const owner = await sendJson(
-        "GET",
-        `${api}/api/v4/user/${element.ownerId}`
-      );
-      const react = await sendJson(
-        "GET",
-        `${api}/api/v2/reaction/${element.imageId}`
-      );
+import http from "./http.js";
+import getAccountInfo from "./account.js";
 
-      main.innerHTML += `
-        <div class="card">
-          <img calss="card-img" src="${imageData}" alt="" />
-          <a class="card-info" href="#">
-            <div class="card-info-top">
-              <h2>${element.name}</h2>
-            </div>
-            <div class="card-info-bottom">
-              <div class="card-bottom-left">${owner.firstName} ${owner.lastName}</div>
-              <div class="card-bottom-right">
-                <i class="bx bxs-heart"></i>
-                <p>${react}</p>
-              </div>
-            </div>
-          </a>
-        </div>
-      `;
-    });
-  } catch (error) {
-    console.log(error);
-  }
+getAccountInfo();
+
+const loadArtwork = async () => {
+  const artworks = await http.send("GET", "/api/v2/artworks/search/getAll");
+  console.log(artworks);
 };
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   fetch("./assets/components/card-item.html")
-//     .then(async (response) => await response.text())
-//     .then(async (data) => {
-//       const cardHolder = (document.querySelector(".card-holder").innerHTML =
-//         data);
-//     });
-// });
+loadArtwork();
