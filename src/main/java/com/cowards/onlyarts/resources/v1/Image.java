1fcb391c22f804f9a72595dda7809d36405b8122
K-R-I-T <datnhse170330@fpt.edu.vn>
2024-03-14 22:51:35 +0700

    feat image
    SWP- #back-end completed
    
    Co-Authored-By: Hoàng Ngọc Thức <84850523+thucnee511@users.noreply.github.com>

--- src/main/java/com/cowards/onlyarts/resources/v1/Image.java
+++ src/main/java/com/cowards/onlyarts/resources/v1/Image.java
@@ -1,11 +1,37 @@
 package com.cowards.onlyarts.resources.v1;
 
 import com.cowards.onlyarts.services.ImageDAO;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("v1/image")
 public class Image {
 
     private static final ImageDAO imageDao = ImageDAO.getInstance();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadImage(String imageData) {
+        imageData = imageData.replaceAll("\"", "");
+        String imageId = imageDao.addImage(imageData);
+        return Response.status(Response.Status.OK)
+                .entity(imageId).build();
+    }
+
+    @GET
+    @Path("{imageId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getImage(@PathParam("imageId") String imageId) {
+        String imageData = imageDao.getImage(imageId);
+        return Response.status(Response.Status.OK)
+                .entity(imageData).build();
+    }
 
 }
--- src/main/java/com/cowards/onlyarts/services/ImageDAO.java
+++ src/main/java/com/cowards/onlyarts/services/ImageDAO.java
@@ -1,11 +1,24 @@
 package com.cowards.onlyarts.services;
 
+import com.cowards.onlyarts.core.CodeGenerator;
 import com.cowards.onlyarts.core.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ImageDAO {
 
     private static final DBContext dbContext = DBContext.getInstance();
     private static ImageDAO instance;
+    private static final String UPLOAD_IMAGE
+            = "INSERT INTO [dbo].[Images]"
+            + "([image_id],[image]) VALUES (?,?)";
+    private static final String GET_IMAGE
+            = "SELECT [image] FROM [dbo].[Images]"
+            + "WHERE [image_id] = ?";
 
     private ImageDAO() {
     }
@@ -15,5 +28,48 @@ public class ImageDAO {
             instance = new ImageDAO();
         }
         return instance;
+    }
+
+    public String addImage(String imageData) {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        String imageId = null;
+        try {
+            conn = dbContext.getConnection();
+            stm = conn.prepareStatement(UPLOAD_IMAGE);
+            imageId = CodeGenerator.generateUUID(20);
+            stm.setString(1, imageId);
+            stm.setString(2, imageData);
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ImageDAO.class.getName())
+                    .log(Level.SEVERE, "Exception found on uploadImage() method", ex);
+        } finally {
+            dbContext.closeStatement(stm);
+        }
+        return imageId;
+    }
+
+    public String getImage(String imageId) {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        String imageData = null;
+        try {
+            conn = dbContext.getConnection();
+            stm = conn.prepareStatement(GET_IMAGE);
+            stm.setString(1, imageId);
+            rs = stm.executeQuery();
+            if (rs.next()) {
+                imageData = rs.getString("image");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ImageDAO.class.getName())
+                    .log(Level.SEVERE, "Exception found on uploadImage() method", ex);
+        } finally {
+            dbContext.closeResultSet(rs);
+            dbContext.closeStatement(stm);
+        }
+        return imageData;
     }
 }
