ebf88aec37b97b3569e2bdb8493e014bedcd83c7
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:03:39 +0700

    refactor: add javadocs to NotificationERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/notification/NotificationERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/notification/NotificationERROR.java
@@ -5,15 +5,33 @@
 package com.cowards.onlyarts.repositories.notification;
 
 /**
- *
- * @author dell
+ * The {@code NotificationERROR} class represents an exception specific to
+ * errors related to notifications. It extends the {@link java.lang.Exception}
+ * class.
  */
 public class NotificationERROR extends Exception {
 
+    /**
+     * Constructs a new {@code NotificationERROR} object with the specified
+     * detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public NotificationERROR(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code NotificationERROR} object with the specified
+     * detail message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public NotificationERROR(String message, Throwable cause) {
         super(message, cause);
     }
