5df1f723311a2cdc3467b67347b91a45dc186443
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:50:49 +0700

    refactor: add javadocs to UserERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/user/UserERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/user/UserERROR.java
@@ -1,11 +1,32 @@
 package com.cowards.onlyarts.repositories.user;
 
+/**
+ * The {@code UserERROR} class represents an exception specific to errors
+ * related to user operations. It extends the {@link java.lang.Exception} class.
+ */
 public class UserERROR extends Exception {
 
+    /**
+     * Constructs a new {@code UserERROR} object with the specified detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public UserERROR(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code UserERROR} object with the specified detail
+     * message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public UserERROR(String message, Throwable cause) {
         super(message, cause);
     }
