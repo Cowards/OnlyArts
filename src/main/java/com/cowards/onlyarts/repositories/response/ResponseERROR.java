ffca603b68694cede2325699532f37fa011a2289
Neru <84850523+thucnee511@users.noreply.github.com>
2024-03-21 18:06:18 +0700

    Revert "Revert "Merge pull request #34 from trthodt/Refactor-add-javadocs-to-ResponseERROR.java-file""
    This reverts commit 664e5a8a3fe9f2c229f77fb86431c9c70598b4f2.

--- src/main/java/com/cowards/onlyarts/repositories/response/ResponseERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/response/ResponseERROR.java
@@ -1,20 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.cowards.onlyarts.repositories.response;
 
 /**
- *
- * @author truon
+ * Custom exception class for handling errors related to responses.
  */
-public class ResponseERROR extends Exception{
-    
+public class ResponseERROR extends Exception {
+
+    /**
+     * Constructs a new ResponseERROR with the specified detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
+     */
     public ResponseERROR(String message) {
         super(message);
     }
+
+    /**
+     * Constructs a new ResponseERROR with the specified detail message and cause.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
+     * @param cause   the cause (which is saved for later retrieval by the getCause() method)
+     */
     public ResponseERROR(String message, Throwable cause) {
         super(message, cause);
     }
-    
 }
