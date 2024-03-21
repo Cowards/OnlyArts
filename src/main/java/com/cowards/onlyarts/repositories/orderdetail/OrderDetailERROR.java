9ebca5faaaa1ef1b442ec961442a63e47e0d84b3
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:27:11 +0700

    refactor: add javadocs to OrderDetailERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/orderdetail/OrderDetailERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/orderdetail/OrderDetailERROR.java
@@ -5,17 +5,33 @@
 package com.cowards.onlyarts.repositories.orderdetail;
 
 /**
- *
- * @author ngocn
+ * The {@code OrderDetailERROR} class represents an exception specific to errors
+ * related to order details. It extends the {@link java.lang.Exception} class.
  */
 public class OrderDetailERROR extends Exception {
 
+    /**
+     * Constructs a new {@code OrderDetailERROR} object with the specified
+     * detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public OrderDetailERROR(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code OrderDetailERROR} object with the specified
+     * detail message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public OrderDetailERROR(String message, Throwable cause) {
         super(message, cause);
     }
-
 }
