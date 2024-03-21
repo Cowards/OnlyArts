5a5debad33090bb700dfa46b4c61f6070bdb477b
K-R-I-T <datnhse170330@fpt.edu.vn>
2024-03-21 19:19:46 +0700

    refactor: add javadocs to Image.java file
    Add javadocs to Image.java file

--- src/main/java/com/cowards/onlyarts/resources/v1/Image.java
+++ src/main/java/com/cowards/onlyarts/resources/v1/Image.java
@@ -10,11 +10,21 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+/**
+ * This class represents the endpoints for image handling, including uploading
+ * and retrieving images.
+ */
 @Path("v1/image")
 public class Image {
 
     private static final ImageDAO imageDao = ImageDAO.getInstance();
 
+    /**
+     * Endpoint for uploading an image.
+     *
+     * @param imageData The image data as a string.
+     * @return Response indicating success or failure of the upload operation.
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -25,6 +35,12 @@ public class Image {
                 .entity(imageId).build();
     }
 
+    /**
+     * Endpoint for retrieving an image.
+     *
+     * @param imageId The unique identifier of the image.
+     * @return Response containing the image data.
+     */
     @GET
     @Path("{imageId}")
     @Produces(MediaType.APPLICATION_JSON)
