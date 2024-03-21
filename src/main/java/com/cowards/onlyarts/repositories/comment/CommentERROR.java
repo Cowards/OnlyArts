d9a7a99fac6be5ba31f0e8e5a564cedcac279b2e
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 19:58:40 +0700

    refactor: add javadocs to CommentERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/comment/CommentERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/comment/CommentERROR.java
@@ -5,17 +5,34 @@
 package com.cowards.onlyarts.repositories.comment;
 
 /**
- *
- * @author Admin
+ * The {@code CommentERROR} class represents an exception specific to errors
+ * related to comments on artworks. It extends the {@link java.lang.Exception}
+ * class.
  */
 public class CommentERROR extends Exception {
 
+    /**
+     * Constructs a new {@code CommentERROR} object with the specified detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public CommentERROR(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code CommentERROR} object with the specified detail
+     * message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public CommentERROR(String message, Throwable cause) {
         super(message, cause);
     }
-
 }
