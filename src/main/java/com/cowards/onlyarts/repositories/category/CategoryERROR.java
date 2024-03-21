c34f93fba4a9da24b850ad5fcd3a43e6eea586c4
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 19:54:46 +0700

    refactor: add javadocs to CategoryERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/category/CategoryERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/category/CategoryERROR.java
@@ -5,17 +5,34 @@
 package com.cowards.onlyarts.repositories.category;
 
 /**
- *
- * @author ngocn
+ * The {@code CategoryERROR} class represents an exception specific to errors
+ * related to category operations. It extends the {@link java.lang.Exception}
+ * class.
  */
 public class CategoryERROR extends Exception {
 
+    /**
+     * Constructs a new {@code CategoryERROR} object with the specified detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public CategoryERROR(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code CategoryERROR} object with the specified detail
+     * message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public CategoryERROR(String message, Throwable cause) {
         super(message, cause);
     }
-
 }
