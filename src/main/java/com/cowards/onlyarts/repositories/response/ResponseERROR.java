4201edfa6d2d7678c017873d80ebd5bd3ca336b7
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:43:09 +0700

    refactor: add javadocs to ResponseERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/response/ResponseERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/response/ResponseERROR.java
@@ -5,16 +5,34 @@
 package com.cowards.onlyarts.repositories.response;
 
 /**
- *
- * @author truon
+ * The {@code ResponseERROR} class represents an exception specific to errors
+ * related to responses to requests. It extends the {@link java.lang.Exception}
+ * class.
  */
-public class ResponseERROR extends Exception{
-    
+public class ResponseERROR extends Exception {
+
+    /**
+     * Constructs a new {@code ResponseERROR} object with the specified detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public ResponseERROR(String message) {
         super(message);
     }
+
+    /**
+     * Constructs a new {@code ResponseERROR} object with the specified detail
+     * message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public ResponseERROR(String message, Throwable cause) {
         super(message, cause);
     }
-    
 }
