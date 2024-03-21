7f43680be1430c9b1e89a7fc2be41f6d3ed570c4
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:01:55 +0700

    refactor: add javadocs to NotificationDTO.java file

--- src/main/java/com/cowards/onlyarts/repositories/notification/NotificationDTO.java
+++ src/main/java/com/cowards/onlyarts/repositories/notification/NotificationDTO.java
@@ -4,11 +4,6 @@
  */
 package com.cowards.onlyarts.repositories.notification;
 
-/**
- *
- * @author dell
- */
-
 import java.sql.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,22 +11,55 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * The {@code NotificationDTO} class represents a data transfer object (DTO) for
+ * managing notifications. It contains fields for the user ID, notification
+ * time, description, and status.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class NotificationDTO {
+
+    /**
+     * The ID of the user associated with the notification.
+     */
     private String userId;
-    private Date  noticeTime;
+
+    /**
+     * The timestamp indicating when the notification occurred.
+     */
+    private Date noticeTime;
+
+    /**
+     * The description or content of the notification.
+     */
     private String description;
+
+    /**
+     * The status of the notification.
+     */
     private int status;
-    
+
+    /**
+     * Checks if the notification has been seen.
+     *
+     * @return {@code true} if the notification has been seen, {@code false}
+     * otherwise.
+     */
     public boolean isSeen() {
         return ((status >> 1) & 1) == 1;
     }
 
+    /**
+     * Checks if the notification has been removed.
+     *
+     * @return {@code true} if the notification has been removed, {@code false}
+     * otherwise.
+     */
     public boolean isRemoved() {
-        return ((status) & 1) == 1;
+        return (status & 1) == 1;
     }
 }
