5e859b5216901964ad3bbebbbbc2b744761dd130
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 19:43:50 +0700

    refactor: add javadocs to ArtworkERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/artwork/ArtworkERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/artwork/ArtworkERROR.java
@@ -1,11 +1,33 @@
 package com.cowards.onlyarts.repositories.artwork;
 
+/**
+ * The {@code ArtworkERROR} class represents an exception specific to errors
+ * related to artwork operations. It extends the {@link java.lang.Exception}
+ * class.
+ */
 public class ArtworkERROR extends Exception {
 
+    /**
+     * Constructs a new {@code ArtworkERROR} object with the specified detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public ArtworkERROR(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code ArtworkERROR} object with the specified detail
+     * message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public ArtworkERROR(String message, Throwable cause) {
         super(message, cause);
     }
