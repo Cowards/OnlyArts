2b6da564be3e07afe7db15ec8b5bdf41ea446548
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:30:09 +0700

    refactor: add javadocs to ReactionERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/reaction/ReactionERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/reaction/ReactionERROR.java
@@ -5,17 +5,34 @@
 package com.cowards.onlyarts.repositories.reaction;
 
 /**
- *
- * @author Admin
+ * The {@code ReactionERROR} class represents an exception specific to errors
+ * related to reactions on artworks. It extends the {@link java.lang.Exception}
+ * class.
  */
 public class ReactionERROR extends Exception {
 
+    /**
+     * Constructs a new {@code ReactionERROR} object with the specified detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public ReactionERROR(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code ReactionERROR} object with the specified detail
+     * message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public ReactionERROR(String message, Throwable cause) {
         super(message, cause);
     }
-
 }
