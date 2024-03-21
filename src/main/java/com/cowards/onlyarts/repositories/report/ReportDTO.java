fdf80c08fad8b145f8aa92ed210091244d87f126
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:31:41 +0700

    refactor: add javadocs to ReportDTO.java file

--- src/main/java/com/cowards/onlyarts/repositories/report/ReportDTO.java
+++ src/main/java/com/cowards/onlyarts/repositories/report/ReportDTO.java
@@ -7,6 +7,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * The {@code ReportDTO} class represents a data transfer object (DTO) for
+ * managing reports on artworks. It contains fields for the report ID, artwork
+ * ID, reporter ID, description, report time, and status.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,17 +19,52 @@ import lombok.ToString;
 @ToString
 public class ReportDTO {
 
+    /**
+     * The ID of the report.
+     */
     private String reportId;
+
+    /**
+     * The ID of the artwork being reported.
+     */
     private String artworkId;
+
+    /**
+     * The ID of the user who reported the artwork.
+     */
     private String reporterId;
+
+    /**
+     * The description or content of the report.
+     */
     private String description;
+
+    /**
+     * The timestamp indicating when the report was made.
+     */
     private Date reportTime;
+
+    /**
+     * The status of the report.
+     */
     private int status;
 
+    /**
+     * Checks if the report has been seen.
+     *
+     * @return {@code true} if the report has been seen, {@code false}
+     * otherwise.
+     */
     public boolean isSeen() {
         return ((status >> 1) & 1) == 1;
     }
 
+    /**
+     * Checks if the report has been responded.
+     *
+     * @return {@code true} if the report has been responded, {@code false}
+     * otherwise.
+     */
     public boolean isResponsed() {
         return (status & 1) == 1;
     }
