0add7a4661cb96e24f495320ca8fd7d963bcb51f
Neru <84850523+thucnee511@users.noreply.github.com>
2024-03-21 18:07:05 +0700

    Revert "Refactor: add javadocs to ResponseDTO.java file"
    This reverts commit 6e1a6d0c0a7ed95ba19137bfc92111479eae622a.

--- src/main/java/com/cowards/onlyarts/repositories/response/ResponseDTO.java
+++ src/main/java/com/cowards/onlyarts/repositories/response/ResponseDTO.java
@@ -7,73 +7,24 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-/**
- * This class represents a Data Transfer Object (DTO) for response information.
- * It contains details such as response ID, request ID, image, description, response time,
- * and status of the response.
- * <p>
- * The status field is used to represent whether the response has been seen or removed.
- * Utility methods are provided to check if the response has been seen or removed.
- * <p>
- * Note: The status is stored as an integer, where bit 0 represents 'seen' and bit 1 represents 'removed'.
- * <p>
- * This class utilizes Lombok annotations for boilerplate code reduction.
- */
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
 public class ResponseDTO {
-
-    /**
-     * The unique identifier for the response.
-     */
     private String responseId;
-
-    /**
-     * The identifier of the request associated with the response.
-     */
     private String requestId;
-
-    /**
-     * The image associated with the response.
-     */
     private String image;
-
-    /**
-     * A description of the response.
-     */
     private String description;
-
-    /**
-     * The timestamp when the response was made.
-     */
     private Date responseTime;
-
-    /**
-     * The status of the response, represented as an integer.
-     * <p>
-     * Bit 0 represents 'seen'.
-     * Bit 1 represents 'removed'.
-     */
-    private int status;
-
-    /**
-     * Checks if the response has been removed.
-     *
-     * @return true if removed, false otherwise.
-     */
+    private int status; //(seen/remove)
     public boolean isRemoved() {
         return ((status >> 1) & 1) == 1;
     }
 
-    /**
-     * Checks if the response has been seen.
-     *
-     * @return true if seen, false otherwise.
-     */
     public boolean isSeen() {
         return (status & 1) == 1;
     }
+    
 }
