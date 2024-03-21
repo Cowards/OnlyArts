3195b10873403be5709bbeaf4f40e8267fb3b172
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:46:59 +0700

    refactor: add javadocs to TokenERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/token/TokenERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/token/TokenERROR.java
@@ -1,24 +1,31 @@
 package com.cowards.onlyarts.repositories.token;
 
 /**
- * Class representing an error related to tokens
+ * The {@code TokenERROR} class represents an exception specific to errors
+ * related to tokens. It extends the {@link java.lang.Exception} class.
  */
 public class TokenERROR extends Exception {
 
     /**
-     * Constructs a TokenError with the specified error message.
+     * Constructs a new {@code TokenERROR} object with the specified detail
+     * message.
      *
-     * @param message The error message.
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
      */
     public TokenERROR(String message) {
         super(message);
     }
 
     /**
-     * Constructs a TokenError with the specified error message and a cause.
+     * Constructs a new {@code TokenERROR} object with the specified detail
+     * message and cause.
      *
-     * @param message The error message.
-     * @param cause The cause of the error.
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
      */
     public TokenERROR(String message, Throwable cause) {
         super(message, cause);
