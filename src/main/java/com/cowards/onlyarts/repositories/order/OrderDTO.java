f855997f39c0cc64443908aca298fdc5c4b471cd
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:22:56 +0700

    refactor: add javadocs to OrderDTO.java file

--- src/main/java/com/cowards/onlyarts/repositories/order/OrderDTO.java
+++ src/main/java/com/cowards/onlyarts/repositories/order/OrderDTO.java
@@ -12,8 +12,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 /**
- *
- * @author Admin
+ * The {@code OrderDTO} class represents a data transfer object (DTO) for
+ * managing orders. It contains fields for the order ID, user ID, status,
+ * payment method, order time, and total price.
  */
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,21 +23,61 @@ import lombok.ToString;
 @ToString
 public class OrderDTO {
 
+    /**
+     * The ID of the order.
+     */
     private String orderId;
+
+    /**
+     * The ID of the user who placed the order.
+     */
     private String userId;
+
+    /**
+     * The status of the order.
+     */
     private int status;
+
+    /**
+     * The payment method used for the order.
+     */
     private String paymentMethod;
+
+    /**
+     * The timestamp indicating when the order was placed.
+     */
     private Date orderTime;
+
+    /**
+     * The total price of the order.
+     */
     private float totalPrice;
 
+    /**
+     * Checks if the order has been removed.
+     *
+     * @return {@code true} if the order has been removed, {@code false}
+     * otherwise.
+     */
     public boolean isRemoved() {
         return (status & 1) == 1;
     }
 
+    /**
+     * Checks if the order has been seen.
+     *
+     * @return {@code true} if the order has been seen, {@code false} otherwise.
+     */
     public boolean isSeen() {
         return ((status >> 2) & 1) == 1;
     }
 
+    /**
+     * Checks if the order has been responded.
+     *
+     * @return {@code true} if the order has been responded, {@code false}
+     * otherwise.
+     */
     public boolean isResponsed() {
         return ((status >> 1) & 1) == 1;
     }
