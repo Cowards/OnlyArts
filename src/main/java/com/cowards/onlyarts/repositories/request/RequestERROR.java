788eb8b5e401a6ed2865ebbe8762ba6221c19ed4
hieuvuanguday <dothehieu.92.20172018@gmail.com>
2024-03-21 20:39:00 +0700

    refactor: add javadocs to RequestERROR.java file

--- src/main/java/com/cowards/onlyarts/repositories/request/RequestERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/request/RequestERROR.java
@@ -5,13 +5,33 @@
 package com.cowards.onlyarts.repositories.request;
 
 /**
- *
- * @author truon
+ * The {@code RequestERROR} class represents an exception specific to errors
+ * related to requests between customers and publishers. It extends the
+ * {@link java.lang.Exception} class.
  */
-public class RequestERROR extends Exception{
+public class RequestERROR extends Exception {
+
+    /**
+     * Constructs a new {@code RequestERROR} object with the specified detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     */
     public RequestERROR(String message) {
         super(message);
     }
+
+    /**
+     * Constructs a new {@code RequestERROR} object with the specified detail
+     * message and cause.
+     *
+     * @param message The detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     */
     public RequestERROR(String message, Throwable cause) {
         super(message, cause);
     }
