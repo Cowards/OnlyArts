900885e50085d7a832d15c1476aa51f49777c337
Neru <84850523+thucnee511@users.noreply.github.com>
2024-03-21 18:06:56 +0700

    Revert "Refactor: add javadocs to ResponseERROR.java file"
    This reverts commit 8a3712d3f9225ca2ef6ba9985ded99d03e836206.

--- src/main/java/com/cowards/onlyarts/repositories/response/ResponseERROR.java
+++ src/main/java/com/cowards/onlyarts/repositories/response/ResponseERROR.java
@@ -1,26 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.cowards.onlyarts.repositories.response;
 
 /**
- * Custom exception class for handling errors related to responses.
+ *
+ * @author truon
  */
-public class ResponseERROR extends Exception {
-
-    /**
-     * Constructs a new ResponseERROR with the specified detail message.
-     *
-     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
-     */
+public class ResponseERROR extends Exception{
+    
     public ResponseERROR(String message) {
         super(message);
     }
-
-    /**
-     * Constructs a new ResponseERROR with the specified detail message and cause.
-     *
-     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
-     * @param cause   the cause (which is saved for later retrieval by the getCause() method)
-     */
     public ResponseERROR(String message, Throwable cause) {
         super(message, cause);
     }
+    
 }
