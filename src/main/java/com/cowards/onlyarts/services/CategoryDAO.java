7ce436de859638cb9f9e26045b6a4421cb0cdb34
K-R-I-T <datnhse170330@fpt.edu.vn>
2024-03-27 12:31:54 +0700

    feat load category
    get category by name

--- src/main/java/com/cowards/onlyarts/services/CategoryDAO.java
+++ src/main/java/com/cowards/onlyarts/services/CategoryDAO.java
@@ -1,6 +1,11 @@
 package com.cowards.onlyarts.services;
 
 import com.cowards.onlyarts.core.DBContext;
+import com.cowards.onlyarts.repositories.category.CategoryDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,6 +13,9 @@ import java.util.logging.Logger;
  * This class provides data access methods for interacting with categories in the database.
  */
 public class CategoryDAO {
+    
+    private static final String GET_CATEGORY_BY_NAME 
+            = "SELECT [cate_id], [cate_name] FROM [dbo].[Categories] WHERE [cate_name] = ?";
 
     private static CategoryDAO instance = null;
     private static final DBContext DB = DBContext.getInstance();
@@ -29,5 +37,28 @@ public class CategoryDAO {
 
     private void logError(String message, Exception ex) {
         Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, message, ex);
+    }
+    
+    public CategoryDTO getCateByName(String cateName) {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        CategoryDTO categoryDTO = new CategoryDTO();
+        try {
+            conn = DB.getConnection();
+            stm = conn.prepareStatement(GET_CATEGORY_BY_NAME);
+            stm.setString(1, cateName);
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                categoryDTO.setCateId(rs.getString(1));
+                categoryDTO.setCateName(rs.getString(2));
+            }
+        } catch (SQLException e) {
+            logError("Exception found on getCateByName() method", e);
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(stm);
+        }
+        return categoryDTO;
     }
 }
